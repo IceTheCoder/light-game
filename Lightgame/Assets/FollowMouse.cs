5d9488c700fc735c7e99187a4f6986ec219b5b6d
IceTheDev2 <cristian.gabriel131408@gmail.com>
2023-03-29 16:49:10 +0300

    The square follows the cursor

---
+++ Lightgame/Assets/FollowMouse.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour
+{
+    public void Update() {
+        Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector2(cursorPos.x, cursorPos.y);
+    }
+}
