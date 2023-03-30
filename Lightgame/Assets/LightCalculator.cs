cb07b5295e4fae29fe77a474b751c93bcfd4919d
IceTheDev2 <cristian.gabriel131408@gmail.com>
2023-03-30 20:52:06 +0300

    Renamed Speedometer to LightCalculator

--- Lightgame/Assets/LightCalculator.cs
+++ Lightgame/Assets/LightCalculator.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class Speedometer : MonoBehaviour
+public class LightCalculator : MonoBehaviour
 {
     private UnityEngine.Rendering.Universal.Light2D light;
     public float speed;
