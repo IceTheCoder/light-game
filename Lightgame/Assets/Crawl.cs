793b432b7f0b2e5f0ebf47a49ab98d15f1a8528b
IceTheDev2 <cristian.gabriel131408@gmail.com>
2023-03-30 14:57:41 +0300

    Script

---
+++ Lightgame/Assets/Crawl.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crawl : MonoBehaviour
+{
+    // Update is called once per frame (Make this not be frame-fixed, use something like Time.Deltatime)
+    void FixedUpdate()
+    {
+        // Pick a random float between 0 and 0.1
+        // Pick another random float between 0 and 0.1
+        // Place them into a Vector2
+        // Change the x- and y-positions of the Transform component of the object by that Vector2
+    }
+}
