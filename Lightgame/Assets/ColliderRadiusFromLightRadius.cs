9e2a4a5882bb9dfc195cf2179b30ea6e32d3fe1b
IceTheDev2 <cristian.gabriel131408@gmail.com>
2023-03-31 14:16:56 +0300

    Empty object acts as light

---
+++ Lightgame/Assets/ColliderRadiusFromLightRadius.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderRadiusFromLightRadius : MonoBehaviour
+{
+    CircleCollider2D circleCollider;
+    UnityEngine.Rendering.Universal.Light2D theLight;
+
+    void Start()
+    {
+        circleCollider = GetComponent<CircleCollider2D>();
+        theLight = GetComponent<UnityEngine.Rendering.Universal.Light2D>();
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        circleCollider.radius = theLight.pointLightOuterRadius;
+    }
+}
