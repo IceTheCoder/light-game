47a54cd1c042731eecb0c5b9ce42f331bef5c27c
IceTheDev2 <cristian.gabriel131408@gmail.com>
2023-04-01 14:46:16 +0300

    THE COLLISION WORKS, I JUST NEEDED A DYANMIC RIGIDBODY

--- Lightgame/Assets/TriangleCollision.cs
+++ Lightgame/Assets/TriangleCollision.cs
@@ -4,12 +4,10 @@ using UnityEngine;
 
 public class TriangleCollision : MonoBehaviour
 {
-    void OnCollisionEnter2D(Collision2D collision)
+    void OnCollisionStay2D(Collision2D collision)
     {
-        if (collision.gameObject.name == "Triangle")
+        if (collision.gameObject.tag == "Triangle")
         {
-            // Collision detected between the 'Light' and 'Triangle' objects.
-            // Do something here, like destroy the 'Triangle' object.
             Debug.Log("Collision detected!");
         }
     }
