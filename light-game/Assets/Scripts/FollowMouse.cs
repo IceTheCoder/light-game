0a4b977c46680d5202865e72855cb57b5672e2af
IceTheDev2 <cristian.gabriel131408@gmail.com>
2023-04-17 17:27:59 +0300

    Document FollowMouse.cs; delete Destroy.cs

--- light-game/Assets/Scripts/FollowMouse.cs
+++ light-game/Assets/Scripts/FollowMouse.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class FollowMouse : MonoBehaviour
 {
+    /// <summary>
+    /// Called once per frame, this method gets the position of the cursor, and sets the object's (the light's)
+    /// position to that of the cursor.
+    /// </summary>
     public void Update() {
         Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.position = new Vector2(cursorPos.x, cursorPos.y);
