79349b063e69ffec2bcce5b033ba41def2bbb7cd
IceTheDev2 <cristian.gabriel131408@gmail.com>
2023-04-24 16:31:25 +0300

    smoothen 'crawling'

--- light-game/Assets/Scripts/Crawl.cs
+++ light-game/Assets/Scripts/Crawl.cs
@@ -2,36 +2,58 @@ using UnityEngine;
 
 public class Crawl : MonoBehaviour
 {
-    public float speed = 2f;
-    public float overlapRadius = 0.5f;
+    public float speed = 2.5f;
+    public float magnitude = 0.2f;
+    public float overlapRadius = 0.1f;
 
+    private Vector2 startPosition;
     private Vector2 targetPosition;
-    private Collider2D[] colliders = new Collider2D[10];
+
+    private Collider2D[] colliders = new Collider2D[0];
 
     void Start()
     {
+        startPosition = transform.position;
         targetPosition = GetRandomTargetPosition();
+        colliders = new Collider2D[1];
     }
 
     void Update()
     {
-        if (Physics2D.OverlapCircleNonAlloc(transform.position, overlapRadius, colliders) > 0)
+        if (Physics2D.OverlapCircleNonAlloc(targetPosition, overlapRadius, colliders) > 0)
+        {
+            targetPosition = GetRandomTargetPosition();
+            return;
+        }
+
+        Vector2 newPosition = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+
+        if (Vector2.Distance(newPosition, targetPosition) < 0.1f)
         {
             targetPosition = GetRandomTargetPosition();
         }
 
-        transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+        // Clamp the target position to the boundaries of the scene
+        float minX = Camera.main.ScreenToWorldPoint(Vector3.zero).x;
+        float maxX = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0)).x;
+        float minY = Camera.main.ScreenToWorldPoint(Vector3.zero).y;
+        float maxY = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height, 0)).y;
+        targetPosition.x = Mathf.Clamp(targetPosition.x, minX, maxX);
+        targetPosition.y = Mathf.Clamp(targetPosition.y, minY, maxY);
 
-        if (Vector2.Distance(transform.position, targetPosition) < 0.1f)
-        {
-            targetPosition = GetRandomTargetPosition();
-        }
+        transform.position = newPosition;
     }
+
 
     private Vector2 GetRandomTargetPosition()
     {
-        Vector2 randomDirection = Random.insideUnitCircle.normalized;
-        float distance = Random.Range(1f, 3f);
-        return (Vector2)transform.position + (randomDirection * distance);
+        Vector2 position = startPosition + Random.insideUnitCircle * magnitude;
+
+        while (Physics2D.OverlapCircleNonAlloc(position, overlapRadius, colliders) > 0)
+        {
+            position = startPosition + Random.insideUnitCircle * magnitude;
+        }
+
+        return position;
     }
 }
