e6320acc1283b6a86112ad81cb7bf16ce928c32e
IceTheDev2 <cristian.gabriel131408@gmail.com>
2023-05-04 19:46:15 +0300

    Update WinGame.cs

--- light-game/Assets/Scripts/WinGame.cs
+++ light-game/Assets/Scripts/WinGame.cs
@@ -12,18 +12,29 @@ public class WinGame : MonoBehaviour
     private float winDelay = 2f;
     private bool canCollide;
 
+    /// <summary>
+    /// 
+    /// </summary>
     private void Start()
     {
         canCollide = false;
         StartCoroutine(WinDelay());
     }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
     IEnumerator WinDelay()
     {
         yield return new WaitForSeconds(winDelay);
         canCollide = true;
     }
 
-
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="collision"></param>
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("WinCondition") && canCollide)
