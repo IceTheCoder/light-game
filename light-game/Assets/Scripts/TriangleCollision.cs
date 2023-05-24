f11311ed3a9557e9dc46ee4b1599b2b558df4279
IceTheDev2 <cristian.gabriel131408@gmail.com>
2023-05-24 11:57:22 +0300

    Debugging

--- light-game/Assets/Scripts/TriangleCollision.cs
+++ light-game/Assets/Scripts/TriangleCollision.cs
@@ -43,6 +43,7 @@ public class TriangleCollision : MonoBehaviour
         if (health < minHealth)
         {
             health = 0f;
+            Debug.Log("Health set to 0.");
         }
     }
 
@@ -69,8 +70,10 @@ public class TriangleCollision : MonoBehaviour
         if (collision.CompareTag("Triangle") && canCollide)
         {
             float[] health0 = new float[] {health - healthChange, 0f};
+            Debug.Log("Health changed.");
             health = health0.Max();
-            textBox.text = "Health: " + (health * 10).ToString("0");
+            textBox.text = "Health: " + (health * 10f).ToString("0");
+            Debug.Log("Text updated.");
         }
     }
 }
