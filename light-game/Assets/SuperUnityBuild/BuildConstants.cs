e66c316662cccf4becaa2bcb766d0de23ccdd17d
IceTheDev2 <cristian.gabriel131408@gmail.com>
2023-06-04 12:47:56 +0300

    Some more configurations

---
+++ light-game/Assets/SuperUnityBuild/BuildConstants.cs
@@ -0,0 +1,41 @@
+using System;
+
+// This file is auto-generated. Do not modify or move this file.
+
+public static class BuildConstants
+{
+    public enum ReleaseType
+    {
+        None,
+        light_game,
+    }
+
+    public enum Platform
+    {
+        None,
+        PC,
+        Linux,
+        Android,
+    }
+
+    public enum Architecture
+    {
+        None,
+        Windows_x86,
+        Linux_x64,
+        Android,
+    }
+
+    public enum Distribution
+    {
+        None,
+    }
+
+    public static readonly DateTime buildDate = new DateTime(638214793946050023);
+    public const string version = "1.0.0.1";
+    public const ReleaseType releaseType = ReleaseType.light_game;
+    public const Platform platform = Platform.Linux;
+    public const Architecture architecture = Architecture.Linux_x64;
+    public const Distribution distribution = Distribution.None;
+}
+
