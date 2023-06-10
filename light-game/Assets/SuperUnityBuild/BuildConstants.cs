2a183d78cc22038d89c8bd4295526ee1c0943fbc
IceTheDev2 <cristian.gabriel131408@gmail.com>
2023-06-10 11:50:35 +0300

    Update BuildConstants.cs

--- light-game/Assets/SuperUnityBuild/BuildConstants.cs
+++ light-game/Assets/SuperUnityBuild/BuildConstants.cs
@@ -34,8 +34,8 @@ public static class BuildConstants
     public static readonly DateTime buildDate = new DateTime(638219945978711924);
     public const string version = "beta-2.9.1.1";
     public const ReleaseType releaseType = ReleaseType.light_game;
-    public const Platform platform = Platform.Linux;
-    public const Architecture architecture = Architecture.Linux_x64;
+    public const Platform platform = Platform.Android;
+    public const Architecture architecture = Architecture.Android;
     public const Distribution distribution = Distribution.None;
 }
 
