3ef113df9a77de31780112974d0c0748841f9267
IceTheDev2 <cristian.gabriel131408@gmail.com>
2023-06-23 10:52:06 +0300

    EnableObject.cs summaries

--- light-game/Assets/Scripts/EnableObject.cs
+++ light-game/Assets/Scripts/EnableObject.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class EnableObject : MonoBehaviour
 {
+    // Should be the player's equipped sword to be enabled after the sword item is picked up.
     public GameObject obj;
 
+    /// <summary>
+    /// Called when the player picks up the sword, this method enables the equipped sword of the player.
+    /// </summary>
     public void Enable()
     {
         obj.SetActive(true);
