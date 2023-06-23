8b478a5aae1f6c2eff7fd49d2f294c7415c6db01
IceTheDev2 <cristian.gabriel131408@gmail.com>
2023-06-23 11:35:28 +0300

    Update ShowText.cs

--- light-game/Assets/Scripts/ShowText.cs
+++ light-game/Assets/Scripts/ShowText.cs
@@ -8,11 +8,17 @@ public class ShowText : MonoBehaviour
     private RectTransform rectTransform;
     public LightCalculator lightCalculator;
 
+    /// <summary>
+    /// Called when the script first loads, this method gets the RectTransform component of the object it sits on.
+    /// </summary>
     private void Start()
     {
         rectTransform = GetComponent<RectTransform>();
     }
 
+    /// <summary>
+    /// Called when a piece of text (such as '+1 SWORD' or the sword cooldown warning), this method sets the position of that text to be visible to the user.
+    /// </summary>
     public void Show()
     {
         if (lightCalculator.won == false) 
