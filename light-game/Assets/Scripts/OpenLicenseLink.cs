f0e826fa95f6f3398def324d6cb96dd0f03ccb30
IceTheDev2 <cristian.gabriel131408@gmail.com>
2023-06-23 11:31:59 +0300

    OpenLicenseLink.cs summary

--- light-game/Assets/Scripts/OpenLicenseLink.cs
+++ light-game/Assets/Scripts/OpenLicenseLink.cs
@@ -4,6 +4,9 @@ public class OpenLicenseLink : MonoBehaviour
 {
     private string licenseURL = "https://github.com/IceTheDev2/light-game/blob/main/LICENSE";
 
+    /// <summary>
+    /// Called when the user clicks the link of the licence, this method loads that link.
+    /// </summary>
     public void OpenLink()
     {
         Application.OpenURL(licenseURL);
