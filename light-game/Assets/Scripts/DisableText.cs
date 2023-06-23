a6787239a857a6f52e551be14aa9fb8f27eee249
IceTheDev2 <cristian.gabriel131408@gmail.com>
2023-06-23 10:50:14 +0300

    DisableText.cs summaries

--- light-game/Assets/Scripts/DisableText.cs
+++ light-game/Assets/Scripts/DisableText.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class DisableText : MonoBehaviour
 {
+    /// <summary>
+    /// Called when the script is first loaded, this method checks, 
+    /// through the Attribution Manager script, if the attributon text has already been shown.
+    /// If it has been shown, it disables the text (so it's only shown once),
+    /// else it lets the text be shown and sets textShown to true to prevent further appearances of the text.
+    /// </summary>
     private void Start()
     {
         if (AttributionManager.textShown)
