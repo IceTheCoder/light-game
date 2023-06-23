214a852b66b108616d1ad63828824a3a53c4279d
IceTheDev2 <cristian.gabriel131408@gmail.com>
2023-06-23 10:46:27 +0300

    DetectPlayerCollision.cs summaries

--- light-game/Assets/Scripts/DetectPlayerCollision.cs
+++ light-game/Assets/Scripts/DetectPlayerCollision.cs
@@ -10,6 +10,13 @@ public class DetectPlayerCollision : MonoBehaviour
     public DisableObject disableObject;
     public ShowTextFor1Sec showTextFor1Sec;
 
+    /// <summary>
+    /// Called upon collision with another trigger object, this method checks if the object is the Light,
+    /// and if that's the case, the method enables the equipped sword underneath the Light,
+    /// shows the text '+1 Sword' for 1 second, and disables the sword item, through various other scripts
+    /// and their methods.
+    /// </summary>
+    /// <param name="collision"></param>
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Light"))
