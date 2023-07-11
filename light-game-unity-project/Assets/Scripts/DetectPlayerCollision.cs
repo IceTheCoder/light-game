36bc0e3750ccc6f786ba3c82cbef820106c0ced2
IceTheDev2 <cristian.gabriel131408@gmail.com>
2023-07-11 15:49:03 +0300

    Finally

--- light-game-unity-project/Assets/Scripts/DetectPlayerCollision.cs
+++ light-game-unity-project/Assets/Scripts/DetectPlayerCollision.cs
@@ -6,9 +6,11 @@ using TMPro;
 
 public class DetectPlayerCollision : MonoBehaviour
 {
+    public float secondsOfText = 1f;
+
     public EnableObject enableObject;
     public DisableObject disableObject;
-    public Show1SwordText show1SwordText;
+    public ShowText showText;
 
     /// <summary>
     /// Called upon collision with another trigger object, this method checks if the object is the Light,
@@ -22,9 +24,15 @@ public class DetectPlayerCollision : MonoBehaviour
         if (collision.CompareTag("Light"))
         {
             enableObject.Enable();
-            show1SwordText.Show();
-            disableObject.Disable();
+            showText.Show();
+            StartCoroutine(HideText());
         }
+    }
 
+    IEnumerator HideText()
+    {
+        yield return new WaitForSeconds(secondsOfText);
+        showText.Hide();
+        disableObject.Disable();
     }
 }
