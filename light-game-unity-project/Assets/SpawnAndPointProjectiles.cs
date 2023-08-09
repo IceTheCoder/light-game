de6df20cc228b045bdccad85059fdd381aa49138
IceTheDev2 <cristian.gabriel131408@gmail.com>
2023-08-09 20:34:25 +0300

    It works! The triangles officially have AI.
    Triangles can now send information to one another: when one of them attacks the player, every other learns of the location and fires, alongside the attacking triangle, (not yet damaging) projectiles towards where they think is the player.

--- light-game-unity-project/Assets/SpawnAndPointProjectiles.cs
+++ light-game-unity-project/Assets/SpawnAndPointProjectiles.cs
@@ -5,27 +5,44 @@ using UnityEngine;
 public class SpawnAndPointProjectiles : MonoBehaviour
 {
     public GameObject projectilePrefab; // Reference to the projectile prefab to be spawned
+    public Transform triangleRotation;
     public float spawnInterval = 1.0f;   // Time interval between spawning projectiles
 
     private float timer = 0.0f;
     private GameObject currentProjectile;
+    private Transform circleTransform;
+    
+    void Start()
+    {
+        circleTransform = GameObject.Find("Circle")?.transform;
+        if (circleTransform == null)
+        {
+            Debug.LogError("Could not find 'Circle' gameObject's transform.");
+        }
+    }
 
     // Update is called once per frame
     void Update()
     {
         timer += Time.deltaTime;
 
-        if (currentProjectile == null && timer >= spawnInterval)
+        // Update the rotation of the spawner to match the negative of triangleRotation.rotation
+        //transform.rotation = Quaternion.Inverse(triangleRotation.rotation);
+
+        if (currentProjectile == null)
         {
             // Spawn a projectile at the position of the spawner
             currentProjectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
 
-            // Calculate the direction towards the (0, 0) coordinates
-            Vector3 direction = new Vector3(0, 0, 0) - currentProjectile.transform.position;
+            if (circleTransform != null)
+            {
+                // Calculate the direction towards the 'Circle' gameObject
+                Vector3 direction = circleTransform.position - currentProjectile.transform.position;
 
-            // Rotate the projectile to point towards the calculated direction
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-            currentProjectile.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+                // Rotate the projectile to point towards the calculated direction
+                float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+                currentProjectile.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            }
 
             // Destroy the projectile after a set time
             Destroy(currentProjectile, 1.0f);
